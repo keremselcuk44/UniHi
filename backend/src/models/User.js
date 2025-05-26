552b3130a77750007c9c7a73ef47314552cbb1fd
acimisk <acimisk@gmail.com>
2025-05-27 00:24:36 +0300

    Kayıt olurken veritabanına eklenecek özellikler yenilendi

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
+  fullName: { type: String, required: true },
+  faculty: { type: String },
+  department: { type: String },
+  studentNumber: { type: String },
+  username: { type: String, required: true, unique: true },
+  email: { type: String }, // eğer email kullanıyorsan burada kalabilir
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
